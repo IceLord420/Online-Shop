7f66d27455437d86ed2991b5a066e0421010352a
IceLord420 <78491489+IceLord420@users.noreply.github.com>
2021-10-24 19:09:56 +0300

    First commit

---
+++ Online Shop/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Online_Shop
+{
+    class Program
+    {
+        public static int nBuyers, nSuppliers, nProducts;
+        static void Main(string[] args)
+        {
+            nBuyers = 100;
+            nSuppliers = 5;
+            nProducts = 20;
+
+            List<double> items = new List<double>();
+
+            for (int i = 0; i < 100000; i++)
+            {
+                Random temp = new Random();
+                items.Add(temp.Next(10000));
+            }
+
+
+            long time1 = Tasks.runSynchronous(items, nBuyers, nSuppliers, nProducts);
+            long time2 = Tasks.runParallel(items, nBuyers, nSuppliers, nProducts);
+
+
+            Console.WriteLine($"synchronous tasks: {time1}");
+            Console.WriteLine($"parallel tasks: {time2}");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Online Shop/Tasks.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
+namespace Online_Shop
+{
+    class Tasks
+    {
+        private static Stopwatch stopwatch = new Stopwatch();
+        
+        public static long runSynchronous(List<double> items, int nBuyers, int nSuppliers, int nProducts)
+        {
+            stopwatch.Restart();
+
+            orderItems(items);
+            restockItems(items);
+
+            stopwatch.Stop();
+            return stopwatch.ElapsedMilliseconds;
+        }
+
+        public static long runParallel(List<double> items, int nBuyers, int nSuppliers, int nProducts)
+        {
+            List<Thread> threads = new List<Thread>();
+            for (int i = 0; i < nBuyers; i++)
+            {
+                threads.Add(new Thread(orderItems));
+            }
+            for (int i = 0; i < nSuppliers; i++)
+            {
+                threads.Add(new Thread(restockItems));
+            }
+
+            stopwatch.Restart();
+            foreach (var thread in threads) thread.Start(items);
+
+            foreach (var thread in threads) thread.Join();
+            stopwatch.Stop();
+            return stopwatch.ElapsedMilliseconds;
+        }
+
+        private static void orderItems(object obj)
+        {
+            List<double> items = (List<double>)obj;
+            for (int i = 0; i < 20; i++)
+            {
+                double temp = items.Count - 1;
+                try
+                {
+                    items.RemoveAt(items.Count - 1);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Nqma kak da se dostapi tozi element");
+                    throw;
+                }
+              
+                Console.WriteLine("order item: " + temp);
+            }
+                
+            
+        }
+
+        private static void restockItems(object obj)
+        {
+            List<double> items = (List<double>)obj;
+            Random rnd = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {
+                items.Add(69420);
+                Console.WriteLine("item added: " + items.Count);
+            }
+        }
+    }
+}
